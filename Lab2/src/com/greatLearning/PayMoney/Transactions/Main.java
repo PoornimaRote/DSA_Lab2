8576eb87e2b6cc6d8d5b3b49ec5cc997a4f565fc
PoornimaRote <142153202+PoornimaRote@users.noreply.github.com>
2023-10-02 14:47:00 +0530

    Add files via upload

---
+++ Lab2/src/com/greatLearning/PayMoney/Transactions/Main.java
@@ -0,0 +1,35 @@
+package com.greatLearning.PayMoney.Transactions;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Transaction t = new Transaction();
+		Scanner scn = new Scanner(System.in);
+		System.out.println("Enter the size of transaction array : ");
+		int size = scn.nextInt();
+		int[]transactions = new int[size];
+		System.out.println("Enter the values of transaction : ");
+		for(int i=0 ; i< size ; i++) {
+			transactions[i] = scn.nextInt();
+		}
+		System.out.println("Enter the total no of targets that needs to be achieved : ");
+		int targetSize = scn.nextInt();
+		
+		for(int i = 0; i < targetSize ; i++) {
+			System.out.println("Enter target value : ");
+			int target = scn.nextInt();
+			int res = t.targetCheck(transactions, target);
+			if(res>=0) {
+				System.out.println("Target achieved after " + (int) (res+1) +" transaction");
+			}
+			else {
+				System.out.println("The target cannot be achieved with the given transaction.");
+			}
+		}
+		scn.close();
+		
+	}
+
+}
---
+++ Lab2/src/com/greatLearning/PayMoney/Transactions/Transaction.java
@@ -0,0 +1,17 @@
+package com.greatLearning.PayMoney.Transactions;
+
+public class Transaction {
+
+	public int targetCheck(int[]transactions, int target) {
+		
+		int sum=0;
+		int n = transactions.length;
+		for(int i=0 ; i< n ; i++) {
+			sum+=transactions[i];
+			if(sum>=target) {
+				return i;
+			}
+		}
+		return-1;
+	}
+}
