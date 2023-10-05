854f6a16c83b2411997e081f25bb8c7825643881
PoornimaRote <142153202+PoornimaRote@users.noreply.github.com>
2023-10-05 13:12:03 +0530

    Update Currency.java

--- Lab2/src/com/greatLearning/Currency/Currency.java
+++ Lab2/src/com/greatLearning/Currency/Currency.java
@@ -1,35 +1,29 @@
 package com.greatLearning.Currency;
 
 public class Currency {
-
-    public void currencyCount(int[] denominations, int amount) {
+        public void currencyCount(int[] denominations, int amount) {
+                  int[] countDenominations = new int[denominations.length];
+                  for (int i = 0; i < denominations.length; i++) {
+                        if (amount >= denominations[i]) {
+                       	        int countCurr = amount / denominations[i];
+                                amount = amount - (denominations[i] * countCurr);
+                                countDenominations[i] = countCurr;
+                                if (amount == 0) {
+                                    break;
+                                }
+                         }
+                   }
        
-        int[] countDenominations = new int[denominations.length];
-
-        for (int i = 0; i < denominations.length; i++) {
-            if (amount >= denominations[i]) {
-                
-            	int countCurr = amount / denominations[i];
-                amount = amount - (denominations[i] * countCurr);
-                countDenominations[i] = countCurr;
-               
-                if (amount == 0) {
-                    break;
-                }
-            }
+                  if(amount==0) {
+        	        System.out.println("Your payment approach in order to give min no of notes will be:");
+        	        for (int i = 0; i < denominations.length; i++) {
+        		        if (countDenominations[i] != 0) {
+        			            System.out.println(denominations[i] + ":" + countDenominations[i]);
+        		        }
+        	         }
+                  }
+                  else {
+        	         System.out.println("Exact amount cannot be paid with the highest possible denominations");
+                  }
         }
-       
-         if(amount==0) {
-        	System.out.println("Your payment approach in order to give min no of notes will be:");
-
-        	for (int i = 0; i < denominations.length; i++) {
-        		if (countDenominations[i] != 0) {
-        			System.out.println(denominations[i] + ":" + countDenominations[i]);
-        		}
-        	}
-         }
-         else {
-        	 System.out.println("Exact amount cannot be paid with the highest possible denominations");
-         }
-   }
 }
